64fba7cce585cead8375527890fd693597a83db3
YegorChevardin <egor03052004@gmail.com>
2023-04-19 21:27:57 +0200

    Initial commit

---
+++ src/test/java/ua/com/epam/lab/yegorchevardin/springboot/giftcertificate/GiftcertificateApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.epam.lab.yegorchevardin.springboot.giftcertificate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftcertificateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
