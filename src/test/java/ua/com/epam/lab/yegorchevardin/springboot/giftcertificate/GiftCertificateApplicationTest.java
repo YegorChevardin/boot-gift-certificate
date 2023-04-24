b7ad9fbe642e3393769b97cef3fa5141f593d79e
YegorChevardin <egor03052004@gmail.com>
2023-04-24 17:19:32 +0200

    Configured spring boot tests

---
+++ src/test/java/ua/com/epam/lab/yegorchevardin/springboot/giftcertificate/GiftCertificateApplicationTest.java
@@ -0,0 +1,10 @@
+package ua.com.epam.lab.yegorchevardin.springboot.giftcertificate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GiftCertificateApplicationTest {
+    @Test
+    void contextLoads() {}
+}
--- src/test/resources/db/database_structure.sql
+++ src/test/resources/db/database_structure.sql
@@ -1,8 +1,3 @@
-SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS, UNIQUE_CHECKS = 0;
-SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0;
-SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE =
-        'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
 -- -----------------------------------------------------
 -- Table `gift_certificates`
 -- -----------------------------------------------------
@@ -21,7 +16,6 @@ CREATE TABLE IF NOT EXISTS `gift_certificates`
     UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
     UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE
 )
-    ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -37,7 +31,6 @@ CREATE TABLE IF NOT EXISTS `tags`
     UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
     UNIQUE INDEX `name_UNIQUE` (`value` ASC) VISIBLE
 )
-    ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -49,20 +42,7 @@ CREATE TABLE IF NOT EXISTS `gift_certificates_tags`
 (
     `gift_certificate_id` INT UNSIGNED NOT NULL,
     `tag_id`              INT UNSIGNED NOT NULL,
-    INDEX `fk_gift_certificates_tags_gift_certificates_idx` (`gift_certificate_id` ASC) VISIBLE,
-    INDEX `fk_gift_certificates_tags_tags1_idx` (`tag_id` ASC) VISIBLE,
-    CONSTRAINT `fk_gift_certificates_tags_gift_certificates`
-        FOREIGN KEY (`gift_certificate_id`)
-            REFERENCES `gift_certificates` (`id`)
-            ON DELETE CASCADE
-            ON UPDATE NO ACTION,
-    CONSTRAINT `fk_gift_certificates_tags_tags1`
-        FOREIGN KEY (`tag_id`)
-            REFERENCES `tags` (`id`)
-            ON DELETE CASCADE
-            ON UPDATE NO ACTION
 )
-    ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -78,7 +58,6 @@ CREATE TABLE IF NOT EXISTS `users`
     UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
     UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE
 )
-    ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -94,15 +73,7 @@ CREATE TABLE IF NOT EXISTS `orders`
     `purchased_at` TIMESTAMP      NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
     UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
-    INDEX `fk_orders_users1_idx` (`user_id` ASC) VISIBLE,
-    CONSTRAINT `fk_orders_users1`
-        FOREIGN KEY (`user_id`)
-            REFERENCES `users` (`id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION
 )
-    ENGINE = InnoDB;
-
 
 -- -----------------------------------------------------
 -- Table `orders_gift_certificates`
@@ -116,19 +87,13 @@ CREATE TABLE IF NOT EXISTS `orders_gift_certificates`
     INDEX `fk_orders_gift_certificates_orders1_idx` (`order_id` ASC) VISIBLE,
     INDEX `fk_orders_gift_certificates_gift_certificates1_idx` (`gift_certificate_id` ASC) VISIBLE,
     CONSTRAINT `fk_orders_gift_certificates_orders1`
-        FOREIGN KEY (`order_id`)
-            REFERENCES `orders` (`id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION,
+    FOREIGN KEY (`order_id`)
+    REFERENCES `orders` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
     CONSTRAINT `fk_orders_gift_certificates_gift_certificates1`
-        FOREIGN KEY (`gift_certificate_id`)
-            REFERENCES `gift_certificates` (`id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION
+    FOREIGN KEY (`gift_certificate_id`)
+    REFERENCES `gift_certificates` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
 )
-    ENGINE = InnoDB;
-
-
-SET SQL_MODE = @OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS = @OLD_UNIQUE_CHECKS;
